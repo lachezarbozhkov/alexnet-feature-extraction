fae82a331fc5b92ee890167d38fcd939fc9536b8
Lachezar Bozhkov <lachezar.bozhkov@gmail.com>
2016-11-27 12:53:27 +0200

    Add test set to download_data

--- download_data.sh
+++ download_data.sh
@@ -1,3 +1,3 @@
 wget https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580a829f_train/train.p
 wget https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580d880c_bvlc-alexnet/bvlc-alexnet.npy
-
+wget https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580ad89e_test/test.p
